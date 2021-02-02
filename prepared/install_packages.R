7b4f382e026b78e0d2939e220e0e65db14ee6dcb
zachbogart <zlbogart@gmail.com>
2021-02-02 13:40:45 -0800

    prepared: classic with some nbex's already enabled

---
+++ prepared/install_packages.R
@@ -0,0 +1,8 @@
+# R Kernel in Jupyter
+install.packages("IRkernel") # for R kernel
+IRkernel::installspec(user = FALSE) # make R kernel visible in jupyter
+
+# Other Packages
+install.packages("tidyverse")
+install.packages("janitor")
+install.packages("glue")
